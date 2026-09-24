02d580a267389f8ecd10d7a8baacbffefcedbb21
Stefan <Apollo71@abv.bg>
2026-09-24 10:14:05 +0300

    fix: jobControllers.js skeleton

--- backend/api-server-starter/controllers/jobControllers.js
+++ backend/api-server-starter/controllers/jobControllers.js
@@ -1,2 +1,26 @@
 const Job = require("../models/jobModel");
 const mongoose = require("mongoose");
+
+
+const getAllJobs= async (req, res) =>{
+
+}
+
+const createJob= async (req, res) =>{
+
+}
+
+
+const getJobbyId= async (req, res) =>{
+
+}
+
+
+const updateJob= async (req, res) =>{
+
+}
+
+
+const deleteJob= async (req, res) =>{
+
+}
